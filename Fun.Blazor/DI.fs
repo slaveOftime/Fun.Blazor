e88d0a93bb2c6620d78eaf27cfbf5b6811728b5e
woo <albertwoo_slaveoftime@hotmail.com>
2021-10-09 14:12:58 +0800

    Support TargetFrameworks for cli

--- Fun.Blazor/DI.fs
+++ Fun.Blazor/DI.fs
@@ -1,17 +1,11 @@
-﻿[<AutoOpen>]
-module Microsoft.Extensions.DependencyInjection.Extensions
+﻿namespace Microsoft.Extensions.DependencyInjection
 
+open System.Runtime.CompilerServices
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.AspNetCore.Components
-open Microsoft.AspNetCore.Components.Web
 open Microsoft.AspNetCore.Components.WebAssembly.Hosting
 open Fun.Blazor
 
-
-type IServiceCollection with
-    member this.AddFunBlazor() =
-        this.AddScoped<IShareStore, ShareStore>()
-            .AddSingleton<IGlobalStore, GlobalStore>()
 
 
 /// This cannot be marked as internal because blazor need it
@@ -24,8 +18,16 @@ type FunBlazorNodeComponent() =
     override this.Render() = this.Node.Node().ToBolero()
 
 
-type WebAssemblyHostBuilder with
-    member this.AddFunBlazorNode(selector: string, node: IFunBlazorNode) =
+[<Extension>]
+type Extensions =
+
+    [<Extension>]
+    static member AddFunBlazor(this: IServiceCollection) =
+        this.AddScoped<IShareStore, ShareStore>()
+            .AddSingleton<IGlobalStore, GlobalStore>()
+
+    [<Extension>]
+    static member AddFunBlazorNode(this: WebAssemblyHostBuilder, selector: string, node: IFunBlazorNode) =
         let parameters = ParameterView.FromDictionary(dict ["Node", box node ])
         this.RootComponents.Add(typeof<FunBlazorNodeComponent>, selector, parameters)
         this
