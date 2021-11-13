6331ad774d2145fe629a4b0b0f117a87fe19f2a4
slaveoftime <albertwoo_slaveoftime@hotmail.com>
2021-11-13 15:54:54 +0800

    Add Observable.ofTask

--- Fun.Blazor/Uitls.fs
+++ Fun.Blazor/Uitls.fs
@@ -1,7 +1,15 @@
 ﻿[<AutoOpen>]
 module Fun.Blazor.Uitls
 
+open System
+open System.Reactive.Linq
+open System.Threading.Tasks
+
 
 let inline (!!) (node: IFunBlazorNode) = GenericFunBlazorNode.create node
 
 let inline genericFunBlazor node = GenericFunBlazorNode.create node
+
+
+module Observable =
+    let ofTask (x: Task<_>) = Observable.FromAsync (fun (token: Threading.CancellationToken) -> x)
