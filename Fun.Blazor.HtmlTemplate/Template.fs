14a29b130ebf3611338591fff08a512aefcd4609
woo <albertwoo_slaveoftime@hotmail.com>
2021-12-13 17:32:02 +0800

    Update doc and demo

--- Fun.Blazor.HtmlTemplate/Template.fs
+++ Fun.Blazor.HtmlTemplate/Template.fs
@@ -5,6 +5,10 @@ open HtmlTemplate.Internals
 
 
 type Template =
+    /// This will generate bolero node tree and put them in caches
+    /// For none standard event you can use @ to indicate it is a callback. @sl-input="...". Normally you do oninput="..."
+    /// For none standard event, you also need to wireup the event argument type by: https://docs.microsoft.com/en-us/aspnet/core/blazor/components/event-handling?view=aspnetcore-6.0#custom-event-arguments
+    /// But it is only supported in aspnet 6. So currently you cannot get the event args very easily.
     static member html(html: FormattableString) =
         caches.GetOrAdd(html.Format.GetHashCode(), (fun _ -> parseNodes html.Format))
         |> buildNodeTree (html.GetArguments())
