116d6f6ae12211799627ce141b675b13488e71f8
slaveoftime <albertwoo_slaveoftime@hotmail.com>
2021-12-14 10:52:39 +0800

    Add AddInstantCallback for better type infer

--- Fun.Blazor/DslAdaptiview.fs
+++ Fun.Blazor/DslAdaptiview.fs
@@ -10,7 +10,8 @@ open Fun.Blazor
 /// This will generate an alist<Node> as a Node parameter.
 /// When the isStatic is not set to true, every time when you call this it will trigger OnParametersSet,
 /// so when you write code like below:
-/// ```fsharp
+/// <example>
+/// <code lang="fsharp">
 ///     adaptiview(){
 ///         1. change hanpend
 ///         adaptiview(){
@@ -18,13 +19,14 @@ open Fun.Blazor
 ///             let! x = cval 123
 ///         }
 ///     }
-/// ```
+/// </code>
+/// </example>
 type AdaptiviewBuilder(?key: obj, ?isStatic: bool) =
     inherit AListBuilder()
 
-    member _.Run (x: alist<Node>) =
-        Bolero.Node.BlazorComponent<AdaptiveComponent>
-            ([
+    member _.Run(x: alist<Node>) =
+        Bolero.Node.BlazorComponent<AdaptiveComponent>(
+            [
                 "Node" => x
                 match isStatic with
                 | Some true -> "IsStatic" => true
@@ -32,51 +34,59 @@ type AdaptiviewBuilder(?key: obj, ?isStatic: bool) =
                 match key with
                 | Some key -> Bolero.Key key
                 | None -> ()
-            ]
-            ,[])
+            ],
+            []
+        )
 
-    member _.Delay (fn: unit -> alist<_>) = fn()
+    member _.Delay(fn: unit -> alist<_>) = fn ()
 
-    member _.Combine (c1, c2) = AList.append c1 c2
+    member _.Combine(c1, c2) = AList.append c1 c2
 
     member _.Yield x = AList.single x
 
-    member _.Zero () = AList.empty
+    member _.Zero() = AList.empty
 
 
 [<Extension>]
 type Extensions =
     [<Extension>]
-    static member Publish (this: cval<'T>, x: 'T) = transact(fun () -> this.Value <- x)
-    
+    static member Publish(this: cval<'T>, x: 'T) = transact (fun () -> this.Value <- x)
+
     [<Extension>]
-    static member Publish (this: cval<'T>, fn: 'T -> 'T) = transact(fun () -> this.Value <- fn this.Value)
-    
+    static member Publish(this: cval<'T>, fn: 'T -> 'T) = transact (fun () -> this.Value <- fn this.Value)
+
     [<Extension>]
-    static member WithSetter (this: cval<'T>) =
-        let setValue x = transact(fun () -> this.Value <- x)
+    static member WithSetter(this: cval<'T>) =
+        let setValue x = transact (fun () -> this.Value <- x)
         this |> AVal.map (fun x -> x, setValue)
+
+
+    /// The action will be triggered immediately
+    /// The default AddCallback will have the same effect, but it has an override with (obj -> unit) which make type infer harder
+    [<Extension>]
+    static member AddInstantCallback(value: aval<'T>, action: 'T -> unit) = value.AddCallback(fun (x: 'T) -> action x)
 
 
     /// This is a helper method to avoid trigger action when you first call this function
     /// Because the default AddCallback will trigger the action immediately
     [<Extension>]
-    static member AddLazyCallback (value: aval<'T>, action: 'T -> unit) =
+    static member AddLazyCallback(value: aval<'T>, action: 'T -> unit) =
         let last = ref ValueNone
 
-        let sub = value.AddMarkingCallback(fun () ->
-            Transaction.Running.Value.AddFinalizer(fun () ->
-                let v = AVal.force value
-                match last.Value with
-                | ValueSome o when DefaultEquality.equals o v -> ()
-                | _ ->
-                    last.Value <- ValueSome v
-                    action v
+        let sub =
+            value.AddMarkingCallback(fun () ->
+                Transaction.Running.Value.AddFinalizer(fun () ->
+                    let v = AVal.force value
+                    match last.Value with
+                    | ValueSome o when DefaultEquality.equals o v -> ()
+                    | _ ->
+                        last.Value <- ValueSome v
+                        action v
+                )
             )
-        )
 
         match Transaction.Running with
-        | ValueSome t -> t.AddFinalizer (fun () -> value |> AVal.force |> ignore)
+        | ValueSome t -> t.AddFinalizer(fun () -> value |> AVal.force |> ignore)
         | ValueNone -> value |> AVal.force |> ignore
 
         sub
@@ -85,7 +95,7 @@ type Extensions =
 [<RequireQualifiedAccess>]
 module Adapt =
     let withSetter (this: cval<'T>) =
-        let setValue x = transact(fun () -> this.Value <- x)
+        let setValue x = transact (fun () -> this.Value <- x)
         this |> AVal.map (fun x -> x, setValue)
 
 
