1697edb17bee57fcefc5f1fd9f14f16bfb648d21
slaveoftime <albertwoo_slaveoftime@hotmail.com>
2021-12-14 10:47:04 +0800

    Set things up in OnInitialized

--- Fun.Blazor.Docs.Wasm/Pages/HelperFunctions/ComponentHookDemo.fs
+++ Fun.Blazor.Docs.Wasm/Pages/HelperFunctions/ComponentHookDemo.fs
@@ -7,52 +7,69 @@ open MudBlazor
 open Fun.Result
 open Fun.Blazor
 
-let componentHookDemo = html.inject (fun (hook: IComponentHook) ->
-    let toggle = hook.UseStore false
-    let count = hook.UseStore 0
-    let threshhold = 3
+let componentHookDemo =
+    html.inject (fun (hook: IComponentHook) ->
+        let toggle = hook.UseStore false
+        let count = hook.UseStore 0
+        let threshhold = 3
 
-    hook.OnFirstAfterRender.Add (fun () ->
-        TimeSpan.FromSeconds 1.
-        |> Observable.interval
-        |> Observable.subscribe (fun _ -> count.Publish (fun x -> x + 1))
-        |> hook.AddDispose
-            
-        count.Observable
-        |> Observable.subscribe (function
-            | GreaterThan threshhold -> toggle.Publish not; count.Publish 0
-            | _ -> ())
-        |> hook.AddDispose)
-    
-    MudPaper'() {
-        Styles [ style.padding 20 ]
-        childContent [
-            html.watch (toggle, fun isToggled -> [
-                MudText'() {
-                    childContent "We can use this hook to subscribe lifecycle event of the component which created by html.inject at the beginening. We can create observable store to have a state management for the current component. After the component disposed all the store will also be disposed."
-                    Typo Typo.subtitle1
-                }
-                MudText'() {
-                    childContent $"After every {threshhold} seconds the toggle will be switched"
-                    Typo Typo.subtitle1
-                    Color Color.Info
-                }
-                MudSwitch'<bool>() {
-                    Checked isToggled
-                    CheckedChanged (fun (x: bool) -> toggle.Publish x)
-                }
-                if isToggled then
-                    MudText'() {
-                        childContent "Toggled successfuly"
-                        Typo Typo.subtitle2
-                        Color Color.Primary
-                    }
-                else
-                    MudText'() {
-                        childContent "Toggled off now"
-                        Color Color.Secondary
-                    }
-                html.watch (count, fun c -> html.text $"Interval Count: {c}")
-            ])
-        ]
-    })
+        hook.OnInitialized.Add(fun () ->
+            toggle.Publish true
+            count.Publish((+) 10)
+        )
+
+        hook.OnFirstAfterRender.Add(fun () ->
+            TimeSpan.FromSeconds 1.
+            |> Observable.interval
+            |> Observable.subscribe (fun _ -> count.Publish(fun x -> x + 1))
+            |> hook.AddDispose
+
+            count.Observable
+            |> Observable.subscribe (
+                function
+                | GreaterThan threshhold ->
+                    toggle.Publish not
+                    count.Publish 0
+                | _ -> ()
+            )
+            |> hook.AddDispose
+        )
+
+        MudPaper'() {
+            Styles [ style.padding 20 ]
+            childContent [
+                html.watch (
+                    toggle,
+                    fun isToggled ->
+                        [
+                            MudText'() {
+                                childContent
+                                    "We can use this hook to subscribe lifecycle event of the component which created by html.inject at the beginening. We can create observable store to have a state management for the current component. After the component disposed all the store will also be disposed."
+                                Typo Typo.subtitle1
+                            }
+                            MudText'() {
+                                childContent $"After every {threshhold} seconds the toggle will be switched"
+                                Typo Typo.subtitle1
+                                Color Color.Info
+                            }
+                            MudSwitch'<bool>() {
+                                Checked isToggled
+                                CheckedChanged(fun (x: bool) -> toggle.Publish x)
+                            }
+                            if isToggled then
+                                MudText'() {
+                                    childContent "Toggled successfuly"
+                                    Typo Typo.subtitle2
+                                    Color Color.Primary
+                                }
+                            else
+                                MudText'() {
+                                    childContent "Toggled off now"
+                                    Color Color.Secondary
+                                }
+                            html.watch (count, (fun c -> html.text $"Interval Count: {c}"))
+                        ]
+                )
+            ]
+        }
+    )
--- Fun.Blazor/DIComponent.fs
+++ Fun.Blazor/DIComponent.fs
@@ -10,33 +10,29 @@ open Microsoft.AspNetCore.Components
 [<AutoOpen>]
 module ServiceProviderExtensions =
     type IServiceProvider with
+
         member sp.GetMultipleServices(depsType: Type, handleNotFoundType) =
             let getSvc x =
                 let svc = sp.GetService(x)
-                if svc = null then handleNotFoundType x
-                else svc
+                if svc = null then handleNotFoundType x else svc
 
             if depsType = typeof<unit> then
                 box ()
             elif Reflection.FSharpType.IsTuple depsType then
-                let svcs =
-                    depsType
-                    |> Reflection.FSharpType.GetTupleElements
-                    |> Array.map getSvc
+                let svcs = depsType |> Reflection.FSharpType.GetTupleElements |> Array.map getSvc
                 Reflection.FSharpValue.MakeTuple(svcs, depsType)
             else
                 getSvc depsType
 
-        member sp.GetMultipleServices(depsType: Type) = sp.GetMultipleServices(depsType, fun _ -> null)
+        member sp.GetMultipleServices(depsType: Type) = sp.GetMultipleServices(depsType, (fun _ -> null))
 
         member sp.GetMultipleServices<'Types>() =
-            sp.GetMultipleServices(typeof<'Types>, fun _ -> null)
-            |> unbox<'Types>
+            sp.GetMultipleServices(typeof<'Types>, (fun _ -> null)) |> unbox<'Types>
 
 
 type DIComponent<'T>() as this =
     inherit FunBlazorComponent()
-    
+
     let mutable node = None
     let mutable shouldRerender = true
 
@@ -55,34 +51,33 @@ type DIComponent<'T>() as this =
             member _.OnDispose = disposeEvent.Publish
             member _.AddDispose dispose = disposes.Add dispose
             member _.AddDisposes ds = disposes.AddRange(ds)
-            member _.StateHasChanged () = this.ForceSetState()
+            member _.StateHasChanged() = this.ForceSetState()
 
-            member _.UseStore<'T> (x: 'T): IStore<'T> =
+            member _.UseStore<'T>(x: 'T) : IStore<'T> =
                 let newStore = new Store<'T>(x)
                 disposes.Add newStore
                 newStore :> IStore<'T>
 
-            member _.UseAVal<'T> (store: IStore<'T>): aval<'T> =
+            member _.UseAVal<'T>(store: IStore<'T>) : aval<'T> =
                 let value' = cval store.Current
-                store.Observable.Subscribe (fun x -> transact (fun _ -> value'.Value <- x)) |> disposes.Add
+                store.Observable.Subscribe(fun x -> transact (fun _ -> value'.Value <- x)) |> disposes.Add
                 value' :> aval<'T>
 
-            member _.UseAVal<'T> (defaultValue: 'T, obser: IObservable<'T>): aval<'T> =
+            member _.UseAVal<'T>(defaultValue: 'T, obser: IObservable<'T>) : aval<'T> =
                 let value' = cval defaultValue
                 obser.Subscribe(fun x -> transact (fun _ -> value'.Value <- x)) |> disposes.Add
                 value' :> aval<'T>
 
-            member _.UseCVal<'T> (store: IStore<'T>): cval<'T> =
+            member _.UseCVal<'T>(store: IStore<'T>) : cval<'T> =
                 let value' = cval store.Current
-                store.Observable.Subscribe (fun x -> transact (fun _ -> value'.Value <- x)) |> disposes.Add
-                value'.AddCallback (fun (x: 'T) -> store.Publish x) |> disposes.Add
+                store.Observable.Subscribe(fun x -> transact (fun _ -> value'.Value <- x)) |> disposes.Add
+                value'.AddCallback(fun (x: 'T) -> store.Publish x) |> disposes.Add
                 value'
         }
 
 
     let handleNotFoundType ty =
-        if ty = typeof<IComponentHook> then box blazorLifecycle
-        else null
+        if ty = typeof<IComponentHook> then box blazorLifecycle else null
 
 
     [<Parameter>]
@@ -95,7 +90,7 @@ type DIComponent<'T>() as this =
 
     [<Inject>]
     member val Services = Unchecked.defaultof<IServiceProvider> with get, set
-    
+
     [<Inject>]
     member val Logger = Unchecked.defaultof<ILogger<DIComponent<'T>>> with get, set
 
@@ -105,20 +100,19 @@ type DIComponent<'T>() as this =
 
 
     override _.Render() =
-        this.Logger.LogDebugForPerf <| fun _ ->
+        this.Logger.LogDebugForPerf(fun () ->
             match node with
-            | None ->
-                let depsType, _ = Reflection.FSharpType.GetFunctionElements(this.RenderFn.GetType())
-                let services = this.Services.GetMultipleServices(depsType, handleNotFoundType) :?> 'T
-                let newNode =  this.RenderFn services
-                node <- newNode |> Some
-                newNode 
+            | None -> Bolero.Empty
+            | Some node -> node
+        )
 
-            | Some node ->
-                node
-        
 
-    override _.OnInitialized () =
+    override _.OnInitialized() =
+        let depsType, _ = Reflection.FSharpType.GetFunctionElements(this.RenderFn.GetType())
+        let services = this.Services.GetMultipleServices(depsType, handleNotFoundType) :?> 'T
+        let newNode = this.RenderFn services
+        node <- newNode |> Some
+
         initialized.Trigger()
 
 
@@ -140,6 +134,6 @@ type DIComponent<'T>() as this =
 
 
     interface IDisposable with
-        member _.Dispose () =
+        member _.Dispose() =
             disposeEvent.Trigger()
             disposes |> Seq.iter (fun x -> x.Dispose())
