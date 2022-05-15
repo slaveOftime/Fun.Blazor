4e6e148678bf34634890ec3ad6d5e94f28767161
binwen <albertwoo_slaveoftime@hotmail.com>
2022-05-15 11:01:54 +0800

    Update docs

--- Fun.Blazor.Docs.Wasm/Demos/AdaptiviewDemo.fs
+++ Fun.Blazor.Docs.Wasm/Demos/AdaptiviewDemo.fs
@@ -16,7 +16,7 @@ let entry =
         }
         adaptiview () {
             let! n2, setN2 = cval(2).WithSetter()
-            div { $"Number1: {n1}. (Will change when click Number1 and Number2)" }
+            div { $"Number1: {n1}. (Will change when click Number1)" }
             div { $"Number2: {n2}. (Will change when click Number2. And will be reset when click Number1)" }
             MudButton'() {
                 OnClick(fun _ -> setN2 (n2 + 1))
@@ -25,8 +25,8 @@ let entry =
         }
         adaptiview (isStatic = true) {
             let! n3, setN3 = cval(3).WithSetter()
-            div { $"Number1: {n1}. (Will change when click Number1 and Number3" }
-            div { $"Number1.Value: {number1.Value}. (Will change when click Number3.)" }
+            div { $"Number1: {n1}. (Will not change, because it is a value type)" }
+            div { $"Number1.Value: {number1.Value}. (Will update to the current Number1 when click Number3. Because it is a reference type)" }
             div { $"Number3: {n3}. (Will change when click Number3" }
             MudButton'() {
                 OnClick(fun _ -> setN3 (n3 + 1))
