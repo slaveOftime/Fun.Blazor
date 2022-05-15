1022a727e2b7f3c463dc6706cf3ab94292d98c36
binwen <albertwoo_slaveoftime@hotmail.com>
2022-05-15 17:34:16 +0800

    Docs: set default markdown theme to avoid flash after SSR

--- Fun.Blazor.Docs.Server/Index.fs
+++ Fun.Blazor.Docs.Server/Index.fs
@@ -43,7 +43,7 @@ type Index() =
                     script { src "_framework/blazor.server.js" }
 
                     stylesheet "css/google-font.css"
-
+                    stylesheet "css/github-markdown-dark.css"
                     stylesheet "css/prism-vsc-dark-plus.css"
                     script { src "https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/components/prism-core.min.js" }
                     script { src "https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/plugins/autoloader/prism-autoloader.min.js" }
