eb1d3411088c64fc2ec1847e6d4edeef2aff2db8
slaveoftime <albertwoo_slaveoftime@hotmail.com>
2022-07-01 18:16:28 +0800

    Fix baseurl for docs

--- build.fsx
+++ build.fsx
@@ -1,16 +1,25 @@
 #r "nuget: BlackFox.Fake.BuildTask,0.1.3"
+#r "nuget: Fake.IO.FileSystem,5.20.4"
 
 #load "docs.fsx"
 
 open BlackFox.Fake
+open Fake.IO
+open Fake.IO.FileSystemOperators
+open Fake.IO.Globbing.Operators
 
 
 fsi.CommandLineArgs |> Array.skip 1 |> BuildTask.setupContextFromArgv
 
 
 
-let generateDocs =
-    BuildTask.create "GenerateDocs" [] { Docs.DocBuilder.build () }
+let generateDocs = BuildTask.create "GenerateDocs" [] { Docs.DocBuilder.build () }
 
+let processGitHubDocs =
+    BuildTask.create "ProcessGitHubDocs" [ generateDocs ] {
+        let targetDir = "Fun.Blazor.Docs.Wasm.Release"
+        !!(targetDir </> "**" </> "index.html")
+        |> Seq.iter (File.applyReplace (fun x -> x.Replace("""<base href="/"/>""", """<base href="/Fun.Blazor.Docs/" /> """)))
+    }
 
 BuildTask.runOrDefault generateDocs
