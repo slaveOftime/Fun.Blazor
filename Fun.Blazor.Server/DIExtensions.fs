e764f93cadaf2f414367662a3614863cdfefad67
binwen <albertwoo_slaveoftime@hotmail.com>
2022-07-24 07:19:59 +0800

    Add more Mapxxx api for writing FunBlazor node to response

--- Fun.Blazor.Server/DIExtensions.fs
+++ Fun.Blazor.Server/DIExtensions.fs
@@ -2,6 +2,7 @@
 
 open System
 open System.IO
+open System.Threading.Tasks
 open System.Text.Encodings.Web
 open System.Runtime.CompilerServices
 open Microsoft.AspNetCore.Http
@@ -37,10 +38,124 @@ type FunBlazorServerExtensions =
 
     /// Build DOM string and write to response
     [<Extension>]
-    static member WriteFunDom(ctx: HttpContext, node: NodeRenderFragment, ?renderMode) = task {
-        let! result = ctx.RenderFragment(typeof<FunFragmentComponent>, defaultArg renderMode RenderMode.Static, dict [ "Fragment", box node ])
-        do! ctx.Response.WriteAsync result
-    }
+    static member WriteFunDom(ctx: HttpContext, node: NodeRenderFragment, ?renderMode) =
+        task {
+            let! result = ctx.RenderFragment(typeof<FunFragmentComponent>, defaultArg renderMode RenderMode.Static, dict [ "Fragment", box node ])
+            ctx.Response.ContentType <- "text/html; charset=UTF-8"
+            do! ctx.Response.WriteAsync result
+        }
+        :> Task
+
+
+    /// 'Services should be a tuple of registered services or unit
+    [<Extension>]
+    static member MapGet<'Services>(builder: IEndpointRouteBuilder, pattern: string, render: 'Services -> Task<NodeRenderFragment>, ?renderMode) =
+        builder.MapGet(
+            pattern,
+            RequestDelegate(fun ctx -> task {
+                let! node = render (ctx.RequestServices.GetMultipleServices<'Services>())
+                do! ctx.WriteFunDom(node, defaultArg renderMode RenderMode.Static)
+            }
+            )
+        )
+
+    /// 'Services should be a tuple of registered services or unit
+    [<Extension>]
+    static member MapGet<'Services>(builder: IEndpointRouteBuilder, pattern: string, render: 'Services -> NodeRenderFragment, ?renderMode) =
+        builder.MapGet(pattern, render >> Task.FromResult, renderMode = defaultArg renderMode RenderMode.Static)
+
+    [<Extension>]
+    static member MapGet(builder: IEndpointRouteBuilder, pattern: string, node: NodeRenderFragment, ?renderMode) =
+        builder.MapGet(pattern, (fun () -> node) >> Task.FromResult, renderMode = defaultArg renderMode RenderMode.Static)
+
+
+    /// 'Services should be a tuple of registered services or unit
+    [<Extension>]
+    static member MapPost<'Services>(builder: IEndpointRouteBuilder, pattern: string, render: 'Services -> Task<NodeRenderFragment>, ?renderMode) =
+        builder.MapPost(
+            pattern,
+            RequestDelegate(fun ctx -> task {
+                let! node = render (ctx.RequestServices.GetMultipleServices<'Services>())
+                do! ctx.WriteFunDom(node, defaultArg renderMode RenderMode.Static)
+            }
+            )
+        )
+
+    /// 'Services should be a tuple of registered services or unit
+    [<Extension>]
+    static member MapPost<'Services>(builder: IEndpointRouteBuilder, pattern: string, render: 'Services -> NodeRenderFragment, ?renderMode) =
+        builder.MapPost(pattern, render >> Task.FromResult, renderMode = defaultArg renderMode RenderMode.Static)
+
+    [<Extension>]
+    static member MapPost(builder: IEndpointRouteBuilder, pattern: string, node: NodeRenderFragment, ?renderMode) =
+        builder.MapPost(pattern, (fun () -> node) >> Task.FromResult, renderMode = defaultArg renderMode RenderMode.Static)
+
+
+    /// 'Services should be a tuple of registered services or unit
+    [<Extension>]
+    static member MapPut<'Services>(builder: IEndpointRouteBuilder, pattern: string, render: 'Services -> Task<NodeRenderFragment>, ?renderMode) =
+        builder.MapPut(
+            pattern,
+            RequestDelegate(fun ctx -> task {
+                let! node = render (ctx.RequestServices.GetMultipleServices<'Services>())
+                do! ctx.WriteFunDom(node, defaultArg renderMode RenderMode.Static)
+            }
+            )
+        )
+
+    /// 'Services should be a tuple of registered services or unit
+    [<Extension>]
+    static member MapPut<'Services>(builder: IEndpointRouteBuilder, pattern: string, render: 'Services -> NodeRenderFragment, ?renderMode) =
+        builder.MapPut(pattern, render >> Task.FromResult, renderMode = defaultArg renderMode RenderMode.Static)
+
+    [<Extension>]
+    static member MapPut(builder: IEndpointRouteBuilder, pattern: string, node: NodeRenderFragment, ?renderMode) =
+        builder.MapPut(pattern, (fun () -> node) >> Task.FromResult, renderMode = defaultArg renderMode RenderMode.Static)
+
+
+    /// 'Services should be a tuple of registered services or unit
+    [<Extension>]
+    static member MapPatch<'Services>(builder: IEndpointRouteBuilder, pattern: string, render: 'Services -> Task<NodeRenderFragment>, ?renderMode) =
+        builder.MapMethods(
+            pattern,
+            [ HttpMethods.Patch ],
+            RequestDelegate(fun ctx -> task {
+                let! node = render (ctx.RequestServices.GetMultipleServices<'Services>())
+                do! ctx.WriteFunDom(node, defaultArg renderMode RenderMode.Static)
+            }
+            )
+        )
+
+    /// 'Services should be a tuple of registered services or unit
+    [<Extension>]
+    static member MapPatch<'Services>(builder: IEndpointRouteBuilder, pattern: string, render: 'Services -> NodeRenderFragment, ?renderMode) =
+        builder.MapPatch(pattern, render >> Task.FromResult, renderMode = defaultArg renderMode RenderMode.Static)
+
+    [<Extension>]
+    static member MapPatch(builder: IEndpointRouteBuilder, pattern: string, node: NodeRenderFragment, ?renderMode) =
+        builder.MapPatch(pattern, (fun () -> node) >> Task.FromResult, renderMode = defaultArg renderMode RenderMode.Static)
+
+
+    /// 'Services should be a tuple of registered services or unit
+    [<Extension>]
+    static member MapDelete<'Services>(builder: IEndpointRouteBuilder, pattern: string, render: 'Services -> Task<NodeRenderFragment>, ?renderMode) =
+        builder.MapDelete(
+            pattern,
+            RequestDelegate(fun ctx -> task {
+                let! node = render (ctx.RequestServices.GetMultipleServices<'Services>())
+                do! ctx.WriteFunDom(node, defaultArg renderMode RenderMode.Static)
+            }
+            )
+        )
+
+    /// 'Services should be a tuple of registered services or unit
+    [<Extension>]
+    static member MapDelete<'Services>(builder: IEndpointRouteBuilder, pattern: string, render: 'Services -> NodeRenderFragment, ?renderMode) =
+        builder.MapDelete(pattern, render >> Task.FromResult, renderMode = defaultArg renderMode RenderMode.Static)
+
+    [<Extension>]
+    static member MapDelete(builder: IEndpointRouteBuilder, pattern: string, node: NodeRenderFragment, ?renderMode) =
+        builder.MapDelete(pattern, (fun () -> node) >> Task.FromResult, renderMode = defaultArg renderMode RenderMode.Static)
 
 
     [<Extension>]
---
+++ Fun.Blazor.Tests/ServerTests.fs
@@ -0,0 +1,119 @@
+﻿module Fun.Blazor.Tests.ServerTests
+
+open System.Net.Http
+open System.Threading
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.TestHost
+open Microsoft.AspNetCore.Routing
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.DependencyInjection
+open Fun.Blazor
+open Xunit
+
+
+let makeTestServer (setRoute: IEndpointRouteBuilder -> unit) =
+    let builder =
+        WebHostBuilder()
+            .UseSetting("test", "123")
+            .ConfigureServices(fun services ->
+                services.AddControllersWithViews() |> ignore
+                services.AddRouting() |> ignore
+            )
+            .Configure(fun builder -> builder.UseRouting().UseEndpoints(setRoute) |> ignore)
+
+    new TestServer(builder)
+
+
+let testNode = div {
+    style { color "red" }
+    "test"
+}
+
+let expectedResponse = """<div style="color: red; ">test</div>"""
+
+
+
+[<Fact>]
+let ``MapGet should work with DI`` () = task {
+    use server =
+        makeTestServer (fun route ->
+            route.MapGet(
+                "/test",
+                fun (config: IConfiguration) -> task {
+                    do! Task.Delay 100
+                    return div { config.GetValue<string>("test") }
+                }
+            )
+            |> ignore
+        )
+
+    use http = server.CreateClient()
+
+    let! actual = http.GetStringAsync("/test")
+    Assert.Equal("<div>123</div>", actual)
+}
+
+
+[<Fact>]
+let ``MapXxx should work`` () = task {
+    use server =
+        makeTestServer (fun route ->
+            route.MapGet("/Get1", (fun () -> testNode)) |> ignore
+            route.MapGet("/Get2", testNode) |> ignore
+            route.MapPost("/Post1", (fun () -> testNode)) |> ignore
+            route.MapPost("/Post2", testNode) |> ignore
+            route.MapPut("/Put1", (fun () -> testNode)) |> ignore
+            route.MapPut("/Put2", testNode) |> ignore
+            route.MapPatch("/Patch1", (fun () -> testNode)) |> ignore
+            route.MapPatch("/Patch2", testNode) |> ignore
+            route.MapDelete("/Delete1", (fun () -> testNode)) |> ignore
+            route.MapDelete("/Delete2", testNode) |> ignore
+        )
+
+    use http = server.CreateClient()
+
+
+    let! actual = http.GetStringAsync("/Get1")
+    Assert.Equal(expectedResponse, actual)
+
+    let! actual = http.GetStringAsync("/Get2")
+    Assert.Equal(expectedResponse, actual)
+
+
+    let! resp = http.PostAsync("/Post1", new StringContent(""))
+    let! actual = resp.Content.ReadAsStringAsync()
+    Assert.Equal(expectedResponse, actual)
+
+    let! resp = http.PostAsync("/Post2", new StringContent(""))
+    let! actual = resp.Content.ReadAsStringAsync()
+    Assert.Equal(expectedResponse, actual)
+
+
+    let! resp = http.PutAsync("/Put1", new StringContent(""))
+    let! actual = resp.Content.ReadAsStringAsync()
+    Assert.Equal(expectedResponse, actual)
+
+    let! resp = http.PutAsync("/Put2", new StringContent(""))
+    let! actual = resp.Content.ReadAsStringAsync()
+    Assert.Equal(expectedResponse, actual)
+
+
+    let! resp = http.PatchAsync("/Patch1", new StringContent(""))
+    let! actual = resp.Content.ReadAsStringAsync()
+    Assert.Equal(expectedResponse, actual)
+
+    let! resp = http.PatchAsync("/Patch2", new StringContent(""))
+    let! actual = resp.Content.ReadAsStringAsync()
+    Assert.Equal(expectedResponse, actual)
+
+
+    let! resp = http.DeleteAsync("/Delete1")
+    let! actual = resp.Content.ReadAsStringAsync()
+    Assert.Equal(expectedResponse, actual)
+
+    let! resp = http.DeleteAsync("/Delete2")
+    let! actual = resp.Content.ReadAsStringAsync()
+    Assert.Equal(expectedResponse, actual)
+}
