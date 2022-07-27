600deab9680a5d0d062f7902ef977fe60ce5a7f9
binwen <albertwoo_slaveoftime@hotmail.com>
2022-07-27 13:02:27 +0800

    Bump version

--- Fun.Blazor.Cli/Types.fs
+++ Fun.Blazor.Cli/Types.fs
@@ -42,7 +42,7 @@ type CodeGenSettings() =
     
     [<Description("Fun.Blazor.Generator version")>]
     [<CommandOption("--generator-version")>]
-    member val GeneratorVersion = "2.1.0" with get, set
+    member val GeneratorVersion = "2.1.1" with get, set
     
     [<Description("Turn on inline option for generated code")>]
     [<CommandOption("--inline")>]
