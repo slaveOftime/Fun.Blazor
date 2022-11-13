0d56abdbdeb0c8c67ace3ee21e3e322353e3215f
slaveoftime <albertwoo_slaveoftime@hotmail.com>
2022-11-13 11:10:28 +0800

    Clean

--- Fun.Blazor/DslCustomElements.fs
+++ Fun.Blazor/DslCustomElements.fs
@@ -2,7 +2,6 @@
 
 open System
 open System.Collections.Concurrent
-open System.Runtime.CompilerServices
 open Microsoft.AspNetCore.Components
 open Microsoft.AspNetCore.Components.Web
 open Fun.Blazor.Operators
@@ -16,31 +15,6 @@ module DslCustomElements =
 
     let mutable internal jsComponentConfig =
         Unchecked.defaultof<IJSComponentConfiguration>
-
-
-    type CustomElement() =
-
-        /// Normally blazorJsSrc should be something like: _framework/blazor.server.js.
-        /// You should before you use custom elements.
-        /// If your application is running in blazor, you can follow Microsoft.AspNetCore.Components.CustomElements related docs. And this call will not be necessary.
-        static member lazyBlazorJs blazorJsSrc =
-            js
-                $"""
-                    window.initBlazor = () => {{
-                        if (!window.Blazor) {{
-                            const CustomElementScript = document.createElement("script")
-                            CustomElementScript.src = "_content/Microsoft.AspNetCore.Components.CustomElements/BlazorCustomElements.js"
-                            document.body.appendChild(CustomElementScript)
-
-                            const blazorScript = document.createElement("script")
-                            blazorScript.src = "{blazorJsSrc}"
-                            document.body.appendChild(blazorScript)
-                        }}
-                    }}
-                """
-
-        /// This will try to call initBlazor.
-        static member internal initBlazorJs = js "if (window.initBlazor) { window.initBlazor() }"
 
 
     /// This is used to register blazor component as web custom element
@@ -62,10 +36,9 @@ module DslCustomElements =
             |> ignore
 
 
-        member _.Run(render: AttrRenderFragment) = html.fragment [ base.Run(render); CustomElement.initBlazorJs ]
+        member _.Run(render: AttrRenderFragment) = base.Run(render)
 
-        member _.Run(render: NodeRenderFragment) = html.fragment [ base.Run(render); CustomElement.initBlazorJs ]
-
+        member _.Run(render: NodeRenderFragment) = base.Run(render)
 
 
     type CustomElementFragment() as this =
@@ -98,10 +71,9 @@ module DslCustomElements =
 [<AutoOpen>]
 module CustomElementExtensions =
 
-    type CustomElement with
+    type CustomElement =
 
         /// Wrap a NodeRenderFragment into a web custom element.
-        /// This is powered by Microsoft.AspNetCore.Components.CustomElements. It will automatically register it.
         /// You will need to call RegisterForFunBlazor.
         /// For example for blazor server: services.AddServerSideBlazor(fun options -> options.RootComponents.RegisterForFunBlazor()).
         /// prerenderNode will put as ChildContent of the current custom element, so when run prerendering it will be used (eg: blazor server).
@@ -109,7 +81,6 @@ module CustomElementExtensions =
 
         /// Wrap a NodeRenderFragment into a web custom element.
         /// 'Services is a tuple of registered services in DI container.
-        /// This is powered by Microsoft.AspNetCore.Components.CustomElements. It will automatically register it.
         /// You will need to call RegisterForFunBlazor.
         /// For example for blazor server: services.AddServerSideBlazor(fun options -> options.RootComponents.RegisterForFunBlazor()).
         /// prerenderNode will put as ChildContent of the current custom element, so when run prerendering it will be used (eg: blazor server).
