edb274e79b8c37ec11fab5a7d602154e7565c32c
slaveoftime <albertwoo_slaveoftime@hotmail.com>
2024-02-06 16:31:51 +0800

    Fix AdaptiviewBuilder for loop

--- Fun.Blazor.Tests/AdaptiveTests.fs
+++ Fun.Blazor.Tests/AdaptiveTests.fs
@@ -11,29 +11,29 @@ open Fun.Blazor
 
 let private createTestContext () =
     let textContext = new TestContext()
-    
-    textContext.Services
-        .AddScoped<INavigationInterception>(fun _ -> Mock.Of<INavigationInterception>())
-        |> ignore
+
+    textContext.Services.AddScoped<INavigationInterception>(fun _ -> Mock.Of<INavigationInterception>())
+    |> ignore
 
     textContext
 
 
 [<Fact>]
 let ``html adaptive tests`` () =
-    let context = createTestContext()
-    
+    let context = createTestContext ()
+
     let store1 = cval 1
     let store2 = cval 1
 
-    let comp = adaptiview(){
-        let! s1 = store1
-        let! s2 = store2
-        html.text $"s1={s1};s2={s2}"
-    }
+    let comp =
+        adaptiview () {
+            let! s1 = store1
+            let! s2 = store2
+            html.text $"s1={s1};s2={s2}"
+        }
 
     let result = context.RenderNode comp
-    
+
     result.MarkupMatches("s1=1;s2=1")
 
     store1.Publish 2
@@ -44,39 +44,53 @@ let ``html adaptive tests`` () =
 
     transact (fun _ ->
         store1.Value <- 8
-        store2.Value <- 9)
+        store2.Value <- 9
+    )
     result.MarkupMatches("s1=8;s2=9")
 
 
 [<Fact>]
 let ``html adaptive complex condition tests`` () =
-    let context = createTestContext()
-    
+    let context = createTestContext ()
+
     let store1 = cval 1
     let store2 = cval 1
 
-    let comp = adaptiview(){
-        let! s1 = store1
-        let! s2 = store2
-        if s1 >= 2 then
-            div { $"s1={s1}" }
-        if s2 >= 2 then
-            div { $"s2={s2}" }
-    }
+    let comp =
+        adaptiview () {
+            let! s1 = store1
+            let! s2 = store2
+            region { if s1 >= 2 then div { $"s1={s1}" } }
+            if s2 >= 2 then div { $"s2={s2}" }
+            for i in 3..4 do
+                div { i }
+        }
 
     let result = context.RenderNode comp
-    
-    Assert.Equal("", result.Markup)
+
+    Assert.Equal("<div>3</div><div>4</div>", result.Markup)
 
     store1.Publish 2
-    result.MarkupMatches("<div>s1=2</div>")
+    result.MarkupMatches
+        """
+            <div>s1=2</div>
+            <div>3</div>
+            <div>4</div>
+        """
 
     store2.Publish 2
     result.MarkupMatches
-        ("""
+        """
             <div>s1=2</div>
             <div>s2=2</div>
-        """)
+            <div>3</div>
+            <div>4</div>
+        """
 
     store2.Publish 1
-    result.MarkupMatches("<div>s1=2</div>")
+    result.MarkupMatches
+        """
+            <div>s1=2</div>
+            <div>3</div>
+            <div>4</div>
+        """
