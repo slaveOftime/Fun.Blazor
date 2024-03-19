5e09294173853de79ad8c17f29d730a444f31eb3
slaveoftime <albertwoo_slaveoftime@hotmail.com>
2024-03-19 13:29:12 +0800

    When i18n tran with arguments but failed with exception, we should fallback to something useful instead of throw or only return key

--- Fun.Blazor.Tests/I18nTests.fs
+++ Fun.Blazor.Tests/I18nTests.fs
@@ -10,6 +10,7 @@ let json =
         "Title": "test app",
         "Messages": {
             "Error1": "Error 1: {0}",
+            "Error3": "Error 3: {0} {1} {2}",
             "Danger": {
                 "Alert": "fail"
             }
@@ -27,11 +28,16 @@ let ``Normal usecases`` () =
 
     Assert.Equal(Some "test app", i18n.tryTran "Title")
     Assert.Equal(None, i18n.tryTran "Titl2")
+    Assert.Equal(None, i18n.tryTran ("Titl2", 1, 2))
 
-    Assert.Throws<System.FormatException>(fun () -> i18n.tran ("Messages.Error1") |> ignore) |> ignore
+    Assert.Equal("Messages.Error1", i18n.tran ("Messages.Error1"))
+    Assert.Equal("Messages.Error3: 1, 2", i18n.tran ("Messages.Error3", 1, 2))
+    Assert.Equal(Some "Error 1: 1", i18n.tryTran ("Messages.Error1", 1, 2, 3))
+    Assert.Equal(None, i18n.tryTran ("Messages.Error3"))
 
     Assert.Equal("Error 1: hi", i18n.tran ("Messages.Error1", "hi"))
     Assert.Equal("Error 1: hi", i18n.tran ("Messages.Error1", "hi", 123))
+    Assert.Equal("Error 1: hi", i18n.tran ("Messages.Error1", "hi", 123, 456))
 
 
 [<Fact>]
